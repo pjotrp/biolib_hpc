56dee6f5fc4bacc513b6696ad47c9d2df64008a3
Pjotr Prins <pjotr.public01@thebird.nl>
2011-07-15 16:55:10 +0200

    Iterate GFF3 records

--- modules/gff3/src/read_gff3_file.d
+++ modules/gff3/src/read_gff3_file.d
@@ -6,6 +6,12 @@ module read_gff3_file;
 
 import std.stdio;
 import std.path;
+import std.string;
+
+/**
+ * This is the basic iterator wich goes over GFF3 records. After  
+ * completing the records it can continue parsing FASTA sequences.
+ */
 
 class ReadGFF3 {
   private File f;
@@ -13,14 +19,32 @@ class ReadGFF3 {
   this(in string filename) {
     f = File(filename,"r");
   }
+
+  string buf;
+  bool hit_fasta = false;
+
+  @property bool empty( ) { 
+    if (buf == "##FASTA") {
+      hit_fasta = true;
+      return true;
+    }
+    return f.eof;
+  }
+  @property ref string front() { return buf; }
+  void popFront() { 
+    do {
+      buf = strip(f.readln()); 
+    } while (buf == "" && !empty());  // skip empty lines
+  }
 }
 
 unittest {
   writeln("Unit test " ~ __FILE__);
   alias std.path.join join;
-  auto fn = dirname(__FILE__) ~ sep ~ join("..","test","data","standard.gff3");
+  auto fn = dirname(__FILE__) ~ sep ~ join("..","test","data","test.gff3");
   writeln("  - reading CSV " ~ fn);
   auto reader = new ReadGFF3(fn);
-  // foreach(mrna ; reader) {
-  // }
+  foreach(rec ; reader) {
+    writeln(rec);
+  }
 }
