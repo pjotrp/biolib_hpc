225cbe884cbe264d4d6dc47005c35906cd7eff88
Pjotr Prins <pjotr.public01@thebird.nl>
2011-07-16 11:36:32 +0200

    FASTA iterator works

--- modules/gff3/src/gff3fasta.d
+++ modules/gff3/src/gff3fasta.d
@@ -1,5 +1,5 @@
 /**
- * Fetch FASTA sequences for GFF3
+ * Fetch FASTA current_sequences for GFF3
  */
 
 module gff3fasta;
@@ -7,6 +7,11 @@ module gff3fasta;
 import std.stdio; 
 import std.string;
 import std.typecons;
+
+/**
+ * Helper class for reading the FASTA records related to a GFF3 file. It uses a 
+ * File object, expecting it to point to the first record. 
+ */
 
 class GFF3Fasta {
 
@@ -16,41 +21,46 @@ class GFF3Fasta {
   this(File f, bool has_fasta) {
     this.f = f;
     this.has_fasta = has_fasta;
+    read_rec(); // read the first record
   }
 
-  string id, next_id = ">first";
-  string seq;
+  string current_id = null; 
+  string current_seq;
+  string lastline = null;  // last line read
 
-  @property bool empty( ) { 
-    return next_id == null || f.eof || !has_fasta;
-  }
-  @property auto front() { return new Tuple!(string,string)(id,seq); }
-  void popFront() { 
-    if (next_id != ">first") {
-      id = next_id;
-      next_id = null;
-    } 
+  private void read_rec() {
+    if (eof()) { current_id = null; return; };
+
+    if (lastline != null) 
+      current_id = lastline;
     else 
-    {
-      do {
-        id = strip(f.readln()); 
-      } while (id == "" && !empty());  // skip empty lines
-    }
-    // make sure it is an id!
-    if (id[0] != '>')
-      throw new Exception("FASTA corrupt " ~ id);
-    writeln("***",id,"---",next_id);
-    seq = "";
+      current_id = strip(f.readln()); 
+
+    // make sure it is an current_id!
+    if (current_id[0] != '>')
+      throw new Exception("FASTA corrupt " ~ current_id);
+
+    current_seq = "";
     string buf;
     do {
       buf = strip(f.readln());
       if (buf.length > 0 && buf[0] == '>') {
-        next_id = buf;
-        return;
+        lastline = buf;
+        return; // done reading rec!
       }
-      seq ~= buf;
-      writeln(buf);
-    } while (!empty()); 
+      current_seq ~= buf;
+    } while (!f.eof()); 
+    lastline = null;
+    writeln(current_id);
+  }
+  auto count = 0;
+  private bool eof() { return !has_fasta || f.eof(); }
+  @property bool empty( ) { if(count++ > 20) return true; writeln("empty", current_id); return current_id == null; }
+  @property auto front() { writeln("front"); return new Tuple!(string,string)(current_id,current_seq); }
+
+  void popFront() { 
+    writeln("popFront");
+    read_rec();
   }
 
 }
